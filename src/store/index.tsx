f0db60fdf4454f167f919169de437577108985b0
cqhaisenbao <fanyu9353@gmail.com>
2022-05-28 21:05:21 +0800

    创建store与settingReducer

---
+++ src/store/index.tsx
@@ -0,0 +1,18 @@
+import {applyMiddleware, combineReducers, createStore} from 'redux'
+import React from "react";
+import {Provider} from "react-redux";
+import settingReducer from "./settingReducer";
+
+const rootReducer = combineReducers({
+    settingReducer
+})
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware()));
+
+export type RootState = ReturnType<typeof store.getState>
+
+export const SettingProvider: React.FC<any> = ({children}) => {
+    return <Provider store={store}>{children}</Provider>
+}
---
+++ src/store/settingReducer/index.ts
@@ -0,0 +1,28 @@
+import {ButtonType} from "antd/es/button";
+
+export interface SettingState {
+    type: ButtonType
+}
+
+interface ActionType {
+    type: string
+    payload?: any
+}
+
+const defaultState: SettingState = {
+    type: 'primary'  // 按钮类型
+}
+
+const settingReducer = (state: SettingState = defaultState, action: ActionType): SettingState => {
+    switch (action.type) {
+        case 'changeType':
+            return {
+                ...state,
+                type: action.payload
+            }
+        default:
+            return state
+    }
+}
+
+export default settingReducer
