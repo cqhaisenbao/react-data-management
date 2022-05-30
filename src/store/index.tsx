1858c0722ea17750a7a9f78b10877bfac64283a8
cqhaisenbao <fanyu9353@gmail.com>
2022-05-30 22:18:08 +0800

    使用redux-persist持久化store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -6,6 +6,15 @@ import userReducer from "./userReducer";
 import {actionLog} from "./middleware/actionLog";
 import {productDetailSlice} from "./productDetail/slice";
 import {changePriceSlice} from "./changePriceReducer/slice";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+import {PersistGate} from "redux-persist/integration/react";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    // whitelist: ['changePriceSlice']
+}
 
 const rootReducer = combineReducers({
     settingReducer,
@@ -14,13 +23,27 @@ const rootReducer = combineReducers({
     changePriceSlice: changePriceSlice.reducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => ([...getDefaultMiddleware(), actionLog])
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => ([...getDefaultMiddleware({
+        serializableCheck: {
+            // Ignore these action types
+            ignoredActions: ['persist/PERSIST'],
+        },
+    }), actionLog])
 });
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>
 
 export const StoreProvider: React.FC<any> = ({children}) => {
-    return <Provider store={store}>{children}</Provider>
+    return <Provider store={store}>
+        {/* @ts-ignore*/}
+        <PersistGate loading={null} persistor={persistor}>
+            {children}
+        </PersistGate>
+    </Provider>
 }
